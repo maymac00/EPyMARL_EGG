af2fe0e65e03304a8172b14db3dcdeac35c746e7
arnau <arnau.mayoral10@gmail.com>
2024-12-03 00:38:24 +0100

    mappo

--- src/test_policies.py
+++ src/test_policies.py
@@ -2,7 +2,7 @@ import time
 
 import numpy as np
 from EthicalGatheringGame import NormalizeReward, StatTracker
-from EthicalGatheringGame.presets import lcarge
+from EthicalGatheringGame.presets import large
 
 from src.modules.agents import RNNNSEGGAgent
 
@@ -14,12 +14,7 @@ import torch as th
 import argparse
 import yaml
 from types import SimpleNamespace
-
-
-env = gym.make("MultiAgentEthicalGathering-large-v1",
-         donation_capacity=10,
-         we=[1, 2.6],
-         efficiency=[0.2, 0.2, 0.67, 0.2, 0.67],)
+from envs.gymma import GymmaWrapper
 
 parser = argparse.ArgumentParser()
 #parser.add_argument("--model_path", type=str)
@@ -29,13 +24,14 @@ args = parser.parse_args()
 
 # load env config as a name space
 env_config = SimpleNamespace(**yaml.safe_load(open(f"config/envs/{args.env_config}.yaml")))
-env_args = env.unwrapped.__dict__
 alg_config = SimpleNamespace(**yaml.safe_load(open(f"config/algs/{args.config}.yaml")))
 # combine namespaces
 config = SimpleNamespace(**vars(env_config), **vars(alg_config))
 
+env = GymmaWrapper(**env_config.env_args)
 
 model_path = "epymarl/results/models/ippo_egg_seed2_MultiAgentEthicalGathering-very-large-demo/40005000"
 
-agents = RNNNSEGGAgent(env.unwrapped.observation_space[0].shape, config)
+
+
 pass
