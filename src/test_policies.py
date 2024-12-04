1d210043498a313cdf5448b855d1215a55e06404
arnau <arnau.mayoral10@gmail.com>
2024-12-04 18:20:06 +0100

    test_policies.py

--- src/test_policies.py
+++ src/test_policies.py
@@ -1,37 +1,46 @@
-import time
-
-import numpy as np
-from EthicalGatheringGame import NormalizeReward, StatTracker
-from EthicalGatheringGame.presets import large
-
-from src.modules.agents import RNNNSEGGAgent
-
-import matplotlib
 import os
+
+from utils.logging import get_logger, Logger
+
+import random
+import matplotlib
 matplotlib.use('TkAgg')
-import gymnasium as gym
-import torch as th
 import argparse
 import yaml
 from types import SimpleNamespace
-from envs.gymma import GymmaWrapper
 
-parser = argparse.ArgumentParser()
-#parser.add_argument("--model_path", type=str)
-parser.add_argument("--config", type=str, default="ippo_egg")
-parser.add_argument("--env-config", type=str, default="egg_large")
-args = parser.parse_args()
+from main import recursive_dict_update
+from run import run_sequential
 
-# load env config as a name space
-env_config = SimpleNamespace(**yaml.safe_load(open(f"config/envs/{args.env_config}.yaml")))
-alg_config = SimpleNamespace(**yaml.safe_load(open(f"config/algs/{args.config}.yaml")))
-# combine namespaces
-config = SimpleNamespace(**vars(env_config), **vars(alg_config))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    #parser.add_argument("--model_path", type=str)
+    parser.add_argument("--config", type=str, default="mappo_egg")
+    parser.add_argument("--env-config", type=str, default="egg_large")
+    parser.add_argument("--model", type=str, default="beegfs/EPyMARL/models/mappo_seed2_MultiAgentEthicalGathering-large-v1_12_04_00_28_135987")
+    test_args = parser.parse_args()
 
-env = GymmaWrapper(**env_config.env_args)
+    # load env config as a name space
+    config = yaml.safe_load(open("config/default.yaml"))
+    recursive_dict_update(config, yaml.safe_load(open(f"config/envs/{test_args.env_config}.yaml")))
+    recursive_dict_update(config, yaml.safe_load(open(f"config/algs/{test_args.config}.yaml")))
 
-model_path = "epymarl/results/models/ippo_egg_seed2_MultiAgentEthicalGathering-very-large-demo/40005000"
+    args = SimpleNamespace(**config)
+    logger = Logger(get_logger())
+
+    args.evaluate = True
+    args.render = False
+    args.use_cuda = False
+    # Get current directory
+    args.checkpoint_path = os.path.join(os.path.dirname(os.getcwd()), test_args.model)
+    args.env_args["seed"] = random.randint(0, 1000000)
+    args.batch_size_run = 1
+    args.device = "cpu"
+    args.test_nepisode = 20
+    args.runner = "episode"
+
+    run_sequential(args, logger)
 
 
 
-pass
+
